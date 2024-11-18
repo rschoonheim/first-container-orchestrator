3e7ae81b66220a3d50b4c07c8b91adee19ce575b
romano schoonheim <stal@romanoschoonheim.nl>
2024-11-18 13:05:32 +0100

    Initial commit.

---
+++ cmd/storage/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+}
+
+//cmd := exec.Cmd{
+//	Path:   "/bin/sh",
+//	Dir:    "/",
+//	Stdin:  os.Stdin,
+//	Stdout: os.Stdout,
+//	Stderr: os.Stderr,
+//	SysProcAttr: &syscall.SysProcAttr{
+//		Chroot:     "/home/romano/Downloads/alpine-minirootfs-3.20.3-x86",
+//		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+//	},
+//}
+//
+//if err := cmd.Run(); err != nil {
+//	fmt.Println(err)
+//}
---
+++ scripts/gen-protoc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+protoc --go_out=. \
+    --go-grpc_out=.  \
+    api/*.proto
