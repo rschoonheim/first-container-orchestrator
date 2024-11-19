ef6e35076521952c9c7a41fe0d6844c93a0bee4b
romano schoonheim <stal@romanoschoonheim.nl>
2024-11-19 12:55:06 +0100

    Turn API into fiber app.

--- go.mod
+++ go.mod
@@ -10,6 +10,16 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
--- src/cmd/api/main.go
+++ src/cmd/api/main.go
@@ -1,48 +1,15 @@
 package main
 
-import (
-	"context"
-	network_v1 "first-container-orchestrator/network/v1"
-	"github.com/google/uuid"
-	"google.golang.org/grpc"
-	"log/slog"
-	"os"
-)
+import "github.com/gofiber/fiber/v2"
 
 func main() {
+	app := fiber.New()
 
-	// gRPC Client
-	//
-	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
-	if err != nil {
-		slog.Error("Failed to connect to server", "error", err)
-		os.Exit(1)
-	}
-	defer conn.Close()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello, World!",
+		})
+	})
 
-	// Create network client
-	//
-	networkClient := network_v1.NewNetworkServiceClient(conn)
-
-	uuidString := uuid.New().String()
-	ctx := context.Background()
-
-	// Create network
-	//
-	network, err := networkClient.CreateNetwork(ctx, &network_v1.CreateNetworkRequest{Id: uuidString})
-	if err != nil {
-		slog.Error("Failed to create network", "error", err)
-		os.Exit(1)
-	}
-
-	slog.Info("Network", "network", network)
-
-	// Get network
-	//
-	network, err = networkClient.GetNetwork(ctx, &network_v1.GetNetworkRequest{Id: uuidString})
-	if err != nil {
-		slog.Error("Failed to get network", "error", err)
-	}
-
-	slog.Info("Network", "network", network)
+	app.Listen(":7331")
 }
